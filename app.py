6d68a8188c57013067e23f17108b040330510b30
Aadi1101 <aadityakomerwar@gmail.com>
2023-08-13 18:55:33 +0530

    API creation

--- app.py
+++ app.py
@@ -1,17 +1,30 @@
-from fastapi import FastAPI
+from flask import Flask,request
 import dill
 import numpy as np
-import gradio as gr
 
 
-app=FastAPI()
-@app.get('/')
+
+app=Flask('__name__')
+@app.route('/')
 def read_main():
     return {"message":"this is your main app"}
 
-def generate_output(input_text:str):
+@app.route('/predict',methods=['GET'])
+def generate_output():
+    json_data = False
+    input_data = request.args.get('data')
+    if input_data==None:
+        input_data = request.get_json()
+        json_data = True
     # input_text = SPX_USO_SLV_EUR_USD_comma_separated_values
-    output_text = [float(item) for item in input_text.split(',')]
+    GLD = process_and_predict(input_text=input_data,json_data=json_data)
+    return {'predicted':GLD}
+
+def process_and_predict(input_text,json_data):
+    if(json_data==True):
+        output_text = [float(item) for item in input_text['data'].split(',')]
+    else:
+        output_text = [float(item) for item in input_text.split(',')]
     with open('src\models\preprocessor.pkl', 'rb') as p:
         preprocessor = dill.load(p)
     output_text = np.array(output_text).reshape(1, -1)
@@ -19,13 +32,6 @@ def generate_output(input_text:str):
     with open('src\models\\best_model.pkl', 'rb') as m:
         model = dill.load(m)
     GLD = model.predict(output_text_dims)
-    return GLD
-
-
-gr_interface = gr.Interface(
-    fn=generate_output,
-    inputs=gr.components.Textbox(placeholder="Enter text here",label="SPX,USO,SLV,EUR/USD(comma_separated_values)"),
-    outputs=gr.components.Textbox(label="GLD"),
-    title="Gold Price Prediction"
-)
-app = gr.mount_gradio_app(app,gr_interface,path='/gradio')
+    return GLD[0]
+if __name__=='__main__':
+    app.run()
